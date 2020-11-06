5c4a758d617c3068133173fae23964f59c52c904
kamal <kamal.aldin.94@gmail.com>
2020-11-06 14:24:32 -0500

    Fix secrets

--- config/generate_prod_secrets.sh
+++ config/generate_prod_secrets.sh
@@ -1,6 +1,6 @@
 ### get backend secrets
-echo $(gcloud secrets versions access 1 --secret="ET_PROD_DJANGO_SECRET") > k8s/prod/secrets/django-secret
-echo $(gcloud secrets versions access 1 --secret="ET_PROD_JWT_SECRET") > k8s/prod/secrets/jwt-secret
-echo $(gcloud secrets versions access 2 --secret="ET_PROD_DATABASE_NAME") > k8s/prod/secrets/database-name-secret
-echo $(gcloud secrets versions access 1 --secret="ET_PROD_DATABASE_USER") > k8s/prod/secrets/database-user-secret
-echo $(gcloud secrets versions access 1 --secret="ET_PROD_DATABASE_PASSWORD") > k8s/prod/secrets/database-password-secret
+gcloud secrets versions access 1 --secret="ET_PROD_DJANGO_SECRET" > k8s/prod/secrets/django-secret
+gcloud secrets versions access 1 --secret="ET_PROD_JWT_SECRET" > k8s/prod/secrets/jwt-secret
+gcloud secrets versions access 2 --secret="ET_PROD_DATABASE_NAME" > k8s/prod/secrets/database-name-secret
+gcloud secrets versions access 1 --secret="ET_PROD_DATABASE_USER" > k8s/prod/secrets/database-user-secret
+gcloud secrets versions access 1 --secret="ET_PROD_DATABASE_PASSWORD" > k8s/prod/secrets/database-password-secret
