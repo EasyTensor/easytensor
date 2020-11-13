59d9eb13ed41109da5c5f97209b02b63dbeaa8bc
kamal <kamal.aldin.94@gmail.com>
2020-11-12 22:58:57 -0500

    Fix email template and add more safeguard to creating controller

--- backend/setup_fixtures.py
+++ backend/setup_fixtures.py
@@ -1,4 +1,11 @@
 #! /local/usr/env python
 # really stupid script to setup one-off starting data
 from allauth.account.models import EmailAddress
-EmailAddress.objects.filter(email="controller@easytensor.com").update(verified=True)
+from uploads.models import User
+
+controller = User.objects.get(username="controller")
+email_address, _ = EmailAddress.objects.get_or_create(
+    email=controller.email, user=controller, user_id=controller.id
+)
+email_address.verified = True
+email_address.save()
--- backend/uploads/models.py
+++ backend/uploads/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 import uuid
+from django.contrib.auth import get_user_model
 
-from django.db.models.deletion import CASCADE
+User = get_user_model()
 
 class Model(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
